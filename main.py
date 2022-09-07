8c3915952b0854a2ba020622d1514561d9256aeb
Ziv <ziv1talias@gmail.com>
2022-09-08 02:07:15 +0300

    add image lines

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import cv2
+import numpy as np
+from numpy import sin, cos, pi
+from matplotlib import image
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":x=1
+
+if __name__ == "__main__":
+    image = cv2.imread('test1.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('Original image', image)
+    matrix_pic = np.copy(image)
+    cv2.imshow('Gray image', gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #print(np.copy(image))
